f6b27c014f162c05e522f84a0eba4f21fdd1b90b
Josiah Hester <jhester911@gmail.com>
2024-10-14 16:00:55 -0400

    Moved the Merge Accounts page underneath the Users tab in wp-admin. Created function to allow access to see and select data from woocommerce, woocommerce-subscriptions, woocommerce-memberships, and also the user's account details.

--- admin/class-wpam-admin.php
+++ admin/class-wpam-admin.php
@@ -109,9 +109,10 @@ class Wpam_Admin {
   }
 
   public function create_admin_page() {
-    add_menu_page(
+    add_submenu_page(
+      'users.php',
       'WP Account Merger',
-      'WP Account Merger',
+      'Merge Accounts',
       'manage_options',
       'wpam',
       array($this, 'render_admin_page'),
@@ -139,17 +140,222 @@ class Wpam_Admin {
       </form>
       <?php
       if (isset($_POST['submit'])) {
-        $source_user_id = $_POST['source_user_id'];
-        $target_user_id = $_POST['target_user_id'];
-
-        $mapped_data = $this->user_data->map_user_data($source_user_id, $target_user_id);
-
-        echo '<h2>Mapped User Data:</h2>';
-        echo '<pre>';
-        print_r($mapped_data);
-        echo '</pre>';
+        $source_id = intval($_POST['source_user_id']);
+        $target_id = intval($_POST['target_user_id']);
+        $user_selection = $this->display_account_details($source_id, $target_id);
+        if (!$user_selection) {
+          echo 'Invalid User IDs';
+          return false;
+        }
+        is_plugin_active('woocommerce/woocommerce.php') ? $this->display_order_choices($source_id, $target_id) : '';
+        is_plugin_active('woocommerce-subscriptions/woocommerce-subscriptions.php') ? $this->display_subscription_choices($source_id, $target_id) : '';
+        is_plugin_active('woocommerce-memberships/woocommerce-memberships.php') ? $this->display_membership_choices($source_id, $target_id) : '';
       } ?>
     </div>
-  <?php
+    <?php
+  }
+
+  public function display_account_details($source_id, $target_id) {
+    $account1 = get_userdata($source_id);
+    $account2 = get_userdata($target_id);
+    if (!$account1 || !$account2) {
+      return false;
+    }
+    ?>
+      <table class="wp-list-table widefat fixed striped">
+        <thead>
+          <tr>
+            <th>Account Details</th>
+            <th>Source</th>
+            <th>Target</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td><input type="radio" name="id" value="<?php echo $account1->ID; ?>"><?php echo $account1->ID; ?></td>
+            <td><input type="radio" name="id" value="<?php echo $account2->ID; ?>" <?php checked(true); ?> ><?php echo $account2->ID; ?></td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td><input type="radio" name="name" value="<?php echo $account1->user_nicename; ?>"><?php echo $account1->user_nicename; ?></td>
+          <td><input type="radio" name="name" value="<?php echo $account2->user_nicename; ?>" <?php checked(true); ?> ><?php echo $account2->user_nicename; ?></td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td><input type="radio" name="email" value="<?php echo $account1->user_email; ?>"><?php echo $account1->user_email; ?></td>
+            <td><input type="radio" name="email" value="<?php echo $account2->user_email; ?>" <?php checked(true); ?> ><?php echo $account2->user_email; ?></td>
+          </tr>
+          <!-- Add more fields as needed -->
+        </tbody>
+    </table>
+    <br>
+    <?php
+    return true;
+  }
+
+  public function display_membership_choices($source_account_id, $target_account_id) {
+    // Retrieve the memberships
+    $source_memberships = $this->user_data->get_user_memberships($source_account_id);
+    $target_memberships = $this->user_data->get_user_memberships($target_account_id);
+
+    // Display the memberships
+    ?>
+    <table class="wp-list-table widefat fixed striped">
+      <thead>
+        <tr>
+          <th>Select</th>
+          <th>Customer ID</th>
+          <th>Membership ID</th>
+          <th>Membership Date</th>
+          <th>Membership Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php $this->check_array_loop_and_display_memberships($source_memberships); ?>
+        <?php $this->check_array_loop_and_display_memberships($target_memberships); ?>
+      </tbody>
+    </table>
+    <br>
+    <?php
+  }
+
+  public function display_subscription_choices($source_account_id, $target_account_id) {
+    // Retrieve the subscriptions
+    $source_subscriptions = $this->user_data->get_user_subscriptions($source_account_id);
+    $target_subscriptions = $this->user_data->get_user_subscriptions($target_account_id);
+
+    // Display the subscriptions
+    ?>
+    <table class="wp-list-table widefat fixed striped">
+      <thead>
+        <tr>
+          <th>Select</th>
+          <th>Customer ID</th>
+          <th>Subscription ID</th>
+          <th>Subscription Date</th>
+          <th>Subscription Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php $this->check_array_loop_and_display_subscriptions($source_subscriptions); ?>
+        <?php $this->check_array_loop_and_display_subscriptions($target_subscriptions); ?>
+      </tbody>
+    </table>
+    <br>
+    <?php
+
+    // Update the merge logic
+    if (isset($_POST['merge_subscriptions'])) {
+      $selected_subscriptions = $_POST['subscriptions'];
+    } else {
+      $selected_subscriptions = array();
+    }
+
+    return $selected_subscriptions ? $selected_subscriptions : false;
+  }
+
+  public function display_order_choices($source_account_id, $target_account_id) {
+    // Retrieve the orders
+    $source_orders = $this->user_data->get_user_orders($source_account_id);
+    $target_orders = $this->user_data->get_user_orders($target_account_id);
+
+    // Display the orders
+    ?>
+    <table class="wp-list-table widefat fixed striped">
+      <thead>
+        <tr>
+          <th>Select</th>
+          <th>Customer ID</th>
+          <th>Order ID</th>
+          <th>Order Date</th>
+          <th>Order Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php $this->check_array_loop_and_display_orders($source_orders); ?>
+        <?php $this->check_array_loop_and_display_orders($target_orders); ?>
+      </tbody>
+    </table>
+    <br>
+    <?php
+
+    // Update the merge logic
+    if (isset($_POST['merge_orders'])) {
+      $selected_orders = $_POST['orders'];
+    } else {
+      $selected_orders = array();
+    }
+
+    return ($selected_orders) ? $selected_orders : false;
+  }
+
+  private function check_array_loop_and_display_orders($array) {
+    if (!$array) {
+      return false;
+    } elseif (is_array($array)) {
+        foreach ($array as $subarray) { ?>
+        <tr>
+      <td><input type="checkbox" name="orders" value="<?php echo $subarray->ID; ?> " <?php checked(true); ?> ></td>
+          <td><?php echo $subarray->get_customer_id(); ?></td>
+          <td><?php echo $subarray->ID; ?></td>
+          <td><?php echo $subarray->order_date; ?></td>
+          <td><?php echo $subarray->order_total; ?></td>
+        </tr>
+    <?php } } else { ?>
+        <tr>
+          <td><input type="checkbox" name="orders" value="<?php echo $array->ID; ?>" <?php checked(true); ?> ></td>
+          <td><?php echo $subarray->get_customer_id(); ?></td>
+          <td><?php echo $array->ID; ?></td>
+          <td><?php echo $array->order_date; ?></td>
+          <td><?php echo $array->order_total; ?></td>
+    </tr> 
+    <?php }
+  }
+
+  private function check_array_loop_and_display_subscriptions($array) {
+    if (!$array) {
+      return false;
+    } elseif (is_array($array)) {
+        foreach ($array as $subarray) { ?>
+        <tr>
+          <td><input type="checkbox" name="orders" value="<?php echo $subarray->ID; ?>" <?php checked(true); ?> ></td>
+          <td><?php echo $subarray->customer_id; ?></td>
+          <td><?php echo $subarray->ID; ?></td>
+          <td><?php echo $subarray->order_date; ?></td>
+          <td><?php echo $subarray->order_total; ?></td>
+        </tr>
+    <?php } } else { ?>
+        <tr>
+          <td><input type="checkbox" name="orders" value="<?php echo $array->ID; ?>" <?php checked(true); ?> ></td>
+          <td><?php echo $array->customer_id; ?></td>
+          <td><?php echo $array->ID; ?></td>
+          <td><?php echo $array->order_date; ?></td>
+          <td><?php echo $array->order_total; ?></td>
+    </tr> 
+    <?php }
+  }
+
+  private function check_array_loop_and_display_memberships($array) {
+    if (!$array) {
+      return false;
+    } elseif (is_array($array)) {
+        foreach ($array as $subarray) { ?>
+        <tr>
+          <td><input type="checkbox" name="orders" value="<?php echo $subarray->id; ?>" <?php checked(true); ?> ></td>
+          <td><?php echo $subarray->user_id; ?></td>
+          <td><?php echo $subarray->id; ?></td>
+          <td><?php echo get_post_meta($subarray->id, '_start_date', true); ?></td>
+          <td><?php echo $subarray->status; ?></td>
+        </tr>
+    <?php } } else { ?>
+        <tr>
+          <td><input type="checkbox" name="orders" value="<?php echo $array->id; ?>" <?php checked(true); ?> ></td>
+          <td><?php echo $array->user_id; ?></td>
+          <td><?php echo $array->id; ?></td>
+          <td><?php echo get_post_meta($subarray->id, '_start_date', true); ?></td>
+          <td><?php echo $array->status; ?></td>
+    </tr> 
+    <?php }
   }
 }
--- includes/class-wpam-user-data.php
+++ includes/class-wpam-user-data.php
@@ -17,74 +17,51 @@ class Wpam_User_Data {
     return $user_data;
   }
 
-  public function get_user_order_ids($user_id) {
+  public function get_user_orders($user_id) {
     $orders = wc_get_orders(array('customer_id' => $user_id));
     if (!$orders) {
       return false;
     }
 
-    $order_ids = array();
+    $orders_data = array();
 
     foreach ($orders as $order) {
-      $order_ids[] = $order->get_id();
+      $data = wc_get_order($order);
+      $orders_data[] = $data;
     }
 
-    return $order_ids;
+    return $orders_data;
   }
 
-  public function get_user_subscription_ids($user_id) {
+  public function get_user_subscriptions($user_id) {
     $subscriptions = wcs_get_subscriptions(array('customer_id' => $user_id));
     if (!$subscriptions) {
       return false;
     }
 
-    $subscription_ids = array();
+    $subscriptions_data = array();
 
     foreach ($subscriptions as $subscription) {
-      $subscription_ids[] = $subscription->get_id();
+      $data = wcs_get_subscription($subscription);
+      $subscriptions_data[] = $data;
     }
 
-    return $subscription_ids;
+    return $subscriptions_data;
   }
 
-  public function get_user_membership_ids($user_id) {
+  public function get_user_memberships($user_id) {
     $memberships = wc_memberships_get_user_memberships($user_id);
     if (!$memberships) {
       return false;
     }
 
-    $membership_ids = array();
+    $memberships_data = array();
 
     foreach ($memberships as $membership) {
-      $membership_ids[] = $membership->get_id();
+      $data = wc_memberships_get_user_membership($membership);
+      $memberships_data[] = $data;
     }
 
-    return $membership_ids;
-  }
-
-  public function get_user_woo_data($user_id) {
-    if (is_plugin_active('woocommerce/woocommerce.php')) {
-      $order_ids = $this->get_user_order_ids($user_id);
-    }
-    $subscription_ids = $this->get_user_subscription_ids($user_id);
-    $membership_ids = $this->get_user_membership_ids($user_id);
-
-  }
-
-  public function map_user_data($source_user_id, $target_user_id) {
-    $source_user_data = $this->wp_am_get_user_data($source_user_id);
-    $target_user_data = $this->wp_am_get_user_data($target_user_id);
-
-    if ((!$target_user_data) || (!$source_user_data)) {
-      return (((!$target_user_data) && (!$source_user_data)) ? "Neither user could be found" : ((!$target_user_data) ? "Target user could not be found" : "Source user could not be found")); 
-    }
-
-    $mapped_data = array();
-
-    $mapped_data['username'] = $target_user_data['username'];
-    $mapped_data['email'] = $target_user_data['email'];
-    $mapped_data['roles'] = $target_user_data['roles'];
-
-    return $this->get_user_woo_data($target_user_id);
+    return $memberships_data;
   }
 }
